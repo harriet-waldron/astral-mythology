66616021fc276c2d5499c5879210fcd5e071a8b3
Harriet Waldron <harriet_waldron@hotmail.com>
2021-06-20 13:06:40 +1200

    fill basic component structure

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,1 +1,19 @@
 // nested ul li, with collapse button, like EDA site
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='nav'>
+      <ul>
+        <li>Home</li>
+        <li>Ancient Greece</li>
+        <ul>
+          <li>Orion</li>
+          <li>Pliedies</li>
+        </ul>
+      </ul>
+    </div>
+  )
+}
+
+export default Nav
